0b80ffee3c4215f6d5bd52de44294c8f2f127d88
Staubgeborener <eric.schroeder@protonmail.com>
2020-05-13 19:10:33 +0200

    add setup.sh

---
+++ setup.sh
@@ -0,0 +1,10 @@
+#update sources
+sudo apt update
+
+#install git
+sudo apt install git
+
+#install asciidoc
+sudo apt install asciidoc asciidoctor
+sudo gem install asciidoctor asciidoctor-pdf --pre
+sudo gem install pygments.rb
