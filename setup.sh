72e506b268843ab29b5cb12f1a9bc91ba0d1b1a1
Staubgeborener <mail@staubgeborener.de>
2020-05-18 16:55:39 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -14,3 +14,6 @@ sudo gem install pygments.rb rouge coderay
 #install AsciidocFX 1.7.3
 wget https://github.com/asciidocfx/AsciidocFX/releases/download/v1.7.3/AsciidocFX_Linux.tar.gz
 tar -xzf AsciidocFX_Linux.tar.gz
+
+#virtualization with VirtualBox - install GuestAdditions
+#sudo mkdir /media/cdrom && sudo mount /dev/cdrom /media/cdrom && sudo sh /media/cdrom/VBoxLinuxAdditions.run
