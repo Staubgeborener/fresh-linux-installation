435343f737633c081eb00e262cbd8c1190d7d3f2
Staubgeborener <mail@staubgeborener.de>
2020-05-18 10:49:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -8,4 +8,9 @@ sudo apt install git
 #install asciidoc
 sudo apt install asciidoc asciidoctor
 sudo gem install asciidoctor asciidoctor-pdf --pre
-sudo gem install pygments.rb
+sudo gem install pygments.rb rouge coderay
+
+#install AsciidocFX 1.7.3
+wget https://github.com/asciidocfx/AsciidocFX/releases/download/v1.7.3/AsciidocFX_Linux.tar.gz
+tar -xzf AsciidocFX_Linux.tar.gz
+sh ./AsciidocFX/AsciidocFX
