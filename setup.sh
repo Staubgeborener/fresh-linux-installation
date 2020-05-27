7573864768bcfcd1515e24b87a4076682d1e9977
Staubgeborener <mail@staubgeborener.de>
2020-05-27 10:25:28 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,5 +15,9 @@ sudo gem install pygments.rb rouge coderay
 wget https://github.com/asciidocfx/AsciidocFX/releases/download/v1.7.3/AsciidocFX_Linux.tar.gz
 tar -xzf AsciidocFX_Linux.tar.gz
 
+#install pycharm
+sudo snap install pycharm-community --classic
+sudo apt-get install python3-distutils
+
 #if virtualization with VirtualBox detected -> install GuestAdditions
 if grep -Fxq "VirtualBox" /sys/class/dmi/id/product_name; then sudo mkdir /media/cdrom && sudo mount /dev/cdrom /media/cdrom && sudo sh /media/cdrom/VBoxLinuxAdditions.run; fi
