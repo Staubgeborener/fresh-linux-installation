9c1a8bea3135ad40a8cbabc0d70d1a8a834bfd22
Staubgeborener <mail@staubgeborener.de>
2020-05-29 09:29:54 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,7 +1,46 @@
 #!/bin/bash
 
+# ANSI escape codes for color
+RED='\033[0;31m'
+NC='\033[0m'
+
+if [[ $EUID -ne 0 ]]; then
+   printf "${RED}Script requires root privileges !!!${NC}\n"
+   exit 1
+fi
+
+while true; do
+    read -p "Is this a forensic machine? That would mean that Sleuthkit, EWF, Xmount, LiME, Volatility, ... (and many more) will also be installed" yn
+    case $yn in
+        [Yy]* )
+        [Nn]* )
+        * ) printf "Please answer yes or no.";;
+    esac
+done
+
+get_vm() {
+        read -p "Ist dies die Plaso [P] oder Xmount/EWF/Sleuthkit/Vol/LiME [X] VM? P oder X eingeben: " vm
+        case $vm in
+                p|P)
+                        return 0
+                        ;;
+                x|X)
+                        return 0
+                        ;;
+                *)
+                        echo -e "\nUngültiger Input\n"
+                        return 1
+                        ;;
+        esac
+}
+
+until get_vm; do : ; done
+
 #update sources and upgrade
 sudo apt update && apt upgrade
+
+#if virtualization with VirtualBox detected -> install GuestAdditions (add GuestAddition.iso in VirtualBox!)
+if grep -Fxq "VirtualBox" /sys/class/dmi/id/product_name; then sudo mkdir /media/cdrom && sudo mount /dev/cdrom /media/cdrom && sudo sh /media/cdrom/VBoxLinuxAdditions.run; fi
 
 #install git
 sudo apt install git
@@ -19,5 +58,46 @@ tar -xzf AsciidocFX_Linux.tar.gz
 sudo snap install pycharm-community --classic
 sudo apt-get install python3-distutils
 
-#if virtualization with VirtualBox detected -> install GuestAdditions
-if grep -Fxq "VirtualBox" /sys/class/dmi/id/product_name; then sudo mkdir /media/cdrom && sudo mount /dev/cdrom /media/cdrom && sudo sh /media/cdrom/VBoxLinuxAdditions.run; fi
+#install python, pip and pycharm
+sudo apt -y install python3.6
+sudo apt -y install python3-pip
+sudo apt -y install python-pip
+pip install --upgrade setuptools
+pip install distorm3
+sudo apt -y install python3-distutils
+sudo snap install pycharm-community --classic
+
+        
+if [[ $yn == "Y" ]] || [[ $vm == "y" ]]; then
+        #install Zeitgeist and Sqilitebrowser
+        sudo apt -y install zeitgeist-explorer
+        sudo apt -y install sqlitebrowser
+        
+        #install Xmount and EWF
+        sudo apt -y install sleuthkit
+        sudo apt -y install mdadm
+        sudo apt -y install ewf-tools
+        sudo apt -y install xmount
+        sudo apt -y install kpartx
+        sudo apt -y install whois
+        sudo apt -y install cryptsetup-bin
+        sudo apt -y install chkrootkit
+        sudo apt -y install rkhunter
+
+        #install lime
+        git clone https://github.com/504ensicsLabs/LiME/
+        cd ./LiME/src
+        make
+        cd ../..
+        
+        #install volatility (v2.6 - Released: December 2016)
+        wget http://downloads.volatilityfoundation.org/releases/2.6/volatility-2.6.zip
+        unzip volatility-2.6.zip
+        sudo rm volatility-2.6.zip
+        #jump through directories because otherwise 'make' fails
+        cd ./volatility-master
+        sudo apt-get install dwarfdump
+        cd ./tools/linux
+        make
+        cd ../../..
+fi
