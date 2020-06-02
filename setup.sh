c1c28ec168b31e08a5440c470499bba69856d7f6
Staubgeborener <mail@staubgeborener.de>
2020-06-02 07:10:34 +0200

    adding support for pacman/arch

--- setup.sh
+++ setup.sh
@@ -26,72 +26,91 @@ get_machine_style() {
 
 until get_machine_style; do : ; done
 
-#update sources and upgrade
-sudo apt -y update && apt -y upgrade
+#it is an arch-like distro?
+APT-pkgmgr=$(which apt-get)
+PACMAN-pkgmgr=$(which pacman)
 
-#if virtualization with VirtualBox detected -> install GuestAdditions (add GuestAddition.iso in VirtualBox!)
-GAs_device=$(lsblk | grep VBox_GA | awk '{ printf $1 }')
-if grep -Fxq "VirtualBox" /sys/class/dmi/id/product_name; then sudo mkdir /media/cdrom && sudo mount /dev/$GAs_device /media/cdrom && sudo sh /media/cdrom/VBoxLinuxAdditions.run; fi
+#APT
+if [ ! -z $APT-pkgmgr ]; then
+    #update sources and upgrade
+    sudo apt -y update && apt -y upgrade
 
-#install git
-sudo apt -y install git
+    #if virtualization with VirtualBox detected -> install GuestAdditions (add GuestAddition.iso in VirtualBox!)
+    GAs_device=$(lsblk | grep VBox_GA | awk '{ printf $1 }')
+    if grep -Fxq "VirtualBox" /sys/class/dmi/id/product_name; then sudo mkdir /media/cdrom && sudo mount /dev/$GAs_device /media/cdrom && sudo sh /media/cdrom/VBoxLinuxAdditions.run; fi
 
-#install asciidoc
-sudo apt -y install asciidoc asciidoctor
-sudo gem install asciidoctor asciidoctor-pdf --pre
-sudo gem install pygments.rb rouge coderay
+    #install git
+    sudo apt -y install git
 
-#install AsciidocFX 1.7.3
-wget https://github.com/asciidocfx/AsciidocFX/releases/download/v1.7.3/AsciidocFX_Linux.tar.gz
-tar -xzf AsciidocFX_Linux.tar.gz
-sudo rm AsciidocFX_Linux.tar.gz
+    #install asciidoc
+    sudo apt -y install asciidoc asciidoctor
+    sudo gem install asciidoctor asciidoctor-pdf --pre
+    sudo gem install pygments.rb rouge coderay
 
-#install python, pip and pycharm
-sudo apt -y install python3.6
-sudo apt -y install python3-pip
-sudo apt -y install python-pip
-sudo apt -y install python3-distutils
-pip install --upgrade setuptools
-pip install distorm3
-sudo snap install pycharm-community --classic
+    #install AsciidocFX 1.7.3
+    wget https://github.com/asciidocfx/AsciidocFX/releases/download/v1.7.3/AsciidocFX_Linux.tar.gz
+    tar -xzf AsciidocFX_Linux.tar.gz
+    sudo rm AsciidocFX_Linux.tar.gz
 
-#forensic machine
-if [ "$yn" != "${yn#[Yy]}" ]; then
-        #install Zeitgeist and Sqilitebrowser
-        sudo apt -y install zeitgeist-explorer
-        sudo apt -y install sqlitebrowser
+    #install python, pip and pycharm
+    sudo apt -y install python3.6
+    sudo apt -y install python3-pip
+    sudo apt -y install python-pip
+    sudo apt -y install python3-distutils
+    pip install --upgrade setuptools
+    pip install distorm3
+    sudo snap install pycharm-community --classic
+    
+#PACMAN
+elif [ ! -z $PACMAN-pkgmgr ]; then
+    #pacman -Sy ....
 
-        #install Sleuthkit, mdadm, EWF, Xmount, Kpartx, cryptsetup and some R00tkit-Hunter
-        sudo apt -y install sleuthkit
-        sudo apt -y install mdadm
-        sudo apt -y install ewf-tools
-        sudo apt -y install xmount
-        sudo apt -y install kpartx
-        sudo apt -y install whois
-        sudo apt -y install cryptsetup-bin
-        sudo apt -y install chkrootkit
-        sudo apt -y install rkhunter
+else
+    printf "${RED}Abort: Cannot determine package manager (neither apt nor pacman)${NC}\n"
+    exit 1
+fi
+
+#forensic machine and apt
+if [ "$yn" != "${yn#[Yy]}" ] && [ ! -z $APT-pkgmgr ]; then
+    #install Zeitgeist and Sqilitebrowser
+    sudo apt -y install zeitgeist-explorer
+    sudo apt -y install sqlitebrowser
+
+    #install Sleuthkit, mdadm, EWF, Xmount, Kpartx, cryptsetup and some R00tkit-Hunter
+    sudo apt -y install sleuthkit
+    sudo apt -y install mdadm
+    sudo apt -y install ewf-tools
+    sudo apt -y install xmount
+    sudo apt -y install kpartx
+    sudo apt -y install whois
+    sudo apt -y install cryptsetup-bin
+    sudo apt -y install chkrootkit
+    sudo apt -y install rkhunter
 	
-        #install plaso (support only for Ubuntu 18.04 LTS (bionic) and 20.04 LTS (focal)!)
-        #sudo apt-get -y install plaso-tools
+    #install plaso (support only for Ubuntu 18.04 LTS (bionic) and 20.04 LTS (focal)!)
+    #sudo apt-get -y install plaso-tools
 
-        #install lime
-        git clone https://github.com/504ensicsLabs/LiME/
-        #jump through directories because otherwise 'make' fails
-        cd ./LiME/src
-        make
-        cd ../..
+    #install lime
+    git clone https://github.com/504ensicsLabs/LiME/
+    #jump through directories because otherwise 'make' fails
+    cd ./LiME/src
+    make
+    cd ../..
 
-        #install volatility (v2.6 - Released: December 2016)
-        wget http://downloads.volatilityfoundation.org/releases/2.6/volatility-2.6.zip
-        unzip volatility-2.6.zip
-        sudo rm volatility-2.6.zip
-        #jump through directories because otherwise 'make' fails
-        cd ./volatility-master
-        sudo apt-get install dwarfdump
-        cd ./tools/linux
-        make
-        cd ../../..
+    #install volatility (v2.6 - Released: December 2016)
+    wget http://downloads.volatilityfoundation.org/releases/2.6/volatility-2.6.zip
+    unzip volatility-2.6.zip
+    sudo rm volatility-2.6.zip
+    #jump through directories because otherwise 'make' fails
+    cd ./volatility-master
+    sudo apt-get install dwarfdump
+    cd ./tools/linux
+    make
+    cd ../../..
+    
+#forensic machine and pacman
+elif [ "$yn" != "${yn#[Yy]}" ] && [ ! -z $APT-pkgmgr ]; then
+    #adding forensic programs for arch
 fi
 
 #edit favorites in gnome
